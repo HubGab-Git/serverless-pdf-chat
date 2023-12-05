5bd5bdb6357a56ce01aa7f86f9a0b1de3262a38f
Hubert Gabryel <hubgab@gmail.com>
2023-12-05 21:26:23 +0100

    test new check

--- frontend/src/components/MultiChatSidebar.tsx
+++ frontend/src/components/MultiChatSidebar.tsx
@@ -7,29 +7,30 @@ interface MultiChatSidebarProps {
   selectedCheckboxes: string[]; // Tablica stringów
 }
 const MultiChatSidebar: React.FC<MultiChatSidebarProps> = ({ onCheckboxChange, selectedCheckboxes }) => {
-  // const [items, setItems] = useState([]);           // Stan dla przechowywania elementów z API
-  // const [selectedItems, setSelectedItems] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(null);
 
   const [checkboxes, setCheckboxes] = useState([]);
-
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Załóżmy, że mamy endpoint API /api/items
         const response = await API.get("serverless-pdf-chat", "/all_web_docs", {});
         console.log("response: "+ JSON.stringify(response, null, 2));
         const data = response.map((item: { filename: string }) => item.filename); 
         console.log("data: "+ JSON.stringify(data, null, 2));
+
         setCheckboxes(data);
+
+        // Zaznacz wszystkie checkboxy i powiadom komponent nadrzędny
+        data.forEach((option: string) => {
+          onCheckboxChange(option);
+        });
       } catch (err: any) {
         console.log(err.message);
       }
     };
 
     fetchData();
-  }, []); // Pusty array jako drugi argument oznacza, że efekt wykona się tylko raz po zamontowaniu komponentu
+  }, []); 
 
   return (
     <div className="col-span-4 h-full">
--- frontend/src/routes/multichat.tsx
+++ frontend/src/routes/multichat.tsx
@@ -1,26 +1,18 @@
 import React, { useState, useEffect, KeyboardEvent } from "react";
-// import { useParams} from "react-router-dom";
 import { API } from "aws-amplify";
 import { Conversation } from "../common/types";
 import MultiChatSidebar from "../components/MultiChatSidebar";
 import ChatMessages from "../components/ChatMessages";
 import md5 from 'crypto-js/md5';
-// import LoadingGrid from "../../public/loading-grid.svg";
 
 const MultiChat: React.FC = () => {
   let conversationId = 'web'
-  // const params = useParams();
-  // const navigate = useNavigate();
   const [conversation, setConversation] = useState<Conversation | null>(null);
-  // const [loading, setLoading] = React.useState<string>("idle");
   const [messageStatus, setMessageStatus] = useState<string>("idle");
-  // const [conversationListStatus, setConversationListStatus] = useState<
-  //   "idle" | "loading"
-  // >("idle");
   const [prompt, setPrompt] = useState("");
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState<string[]>([]);
 
   const fetchData = async (conversationid = conversationId) => {
-    // setLoading("loading");
     const conversation = await API.get(
       "serverless-pdf-chat",
       `/doc/web/${conversationid}`,
@@ -28,7 +20,6 @@ const MultiChat: React.FC = () => {
     );
     console.log("odpowiedz: "+ JSON.stringify(conversation, null, 2))
     setConversation(conversation);
-    // setLoading("idle");
   };
 
   useEffect(() => {
@@ -38,24 +29,6 @@ const MultiChat: React.FC = () => {
   const handlePromptChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPrompt(event.target.value);
   };
-
-  // const addConversation = async () => {
-  //   // setConversationListStatus("loading");
-  //   const newConversation = await API.post(
-  //     "serverless-pdf-chat",
-  //     `/doc/web`,
-  //     {}
-  //   );
-  //   fetchData(newConversation.conversationid);
-  //   navigate(`/doc/web/${newConversation.conversationid}`);
-  //   // setConversationListStatus("idle");
-  // };
-
-  // const switchConversation = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   const targetButton = e.target as HTMLButtonElement;
-  //   navigate(`/doc/web/${targetButton.id}`);
-  //   fetchData(targetButton.id);
-  // };
 
   const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key == "Enter") {
@@ -101,45 +74,7 @@ const MultiChat: React.FC = () => {
     setMessageStatus("idle");
   };
 
-  // let conver = {
-  //   "conversationid": "jsrKAdW4svxwHRqnMHuD2K",
-  //   "document": {
-  //     "filename": "Tamponada_serca",
-  //     "conversations": [
-  //       {
-  //         "conversationid": "jsrKAdW4svxwHRqnMHuD2K",
-  //         "created": "2023-11-29T16:41:00.444465Z"
-  //       }
-  //     ],
-  //     "docstatus": "READY",
-  //     "filesize": "0",
-  //     "userid": "d0c36275-5f90-4418-b4d0-81c92df094e7",
-  //     "documentid": "2ecfzYwKWe3c73hpzhh9JZ",
-  //     "created": "2023-11-29T16:41:00.444465Z",
-  //     "pages": "0"
-  //   },
-  //   "messages": [
-  //     {
-  //       "type": "human",
-  //       "data": {
-  //         "type": "human",
-  //         "content": "Podaj Objawy podmiotowe Tamponady serca",
-  //         "additional_kwargs": {},
-  //         "example": false
-  //       }
-  //     },
-  //     {
-  //       "type": "ai",
-  //       "data": {
-  //         "type": "ai",
-  //         "content": " Zgodnie z podanym fragmentem, objawy podmiotowe tamponady serca to:\n\n- Duszność nasilająca się po przyjęciu pozycji leżącej \n- Zmniejszona tolerancja wysiłku fizycznego\n- Niekiedy kaszel \n- Dysfagia  \n- Omdlenie lub stan przedomdleniowy",
-  //         "additional_kwargs": {},
-  //         "example": false
-  //       }
-  //     }
-  //   ]}
 
-    const [selectedCheckboxes, setSelectedCheckboxes] = useState<string[]>([]);
 
 
     const handleCheckboxChange = (option: string) => {
